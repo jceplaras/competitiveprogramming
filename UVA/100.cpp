2a01bcb8e1dace804c60e0b845911e7d4b8296e1
jceplaras <jamesplaras@gmail.com>
2014-10-09 15:09:54 +0800

    UVA 100

--- UVA/100.cpp
+++ UVA/100.cpp
@@ -1,3 +1,7 @@
+/*Author:       James Carlo Plaras
+ * UVA Problem: 100
+ */
+
 #include <algorithm>
 #include <cmath>
 #include <cctype>
@@ -14,7 +18,6 @@
 #define FORN(i,N) for (int i = 0; i < N; i++)
 #define FORD(i,a,b) for (int i = a; i >= b; i--)
 
-
 using namespace std;
 
 typedef long long ll;
@@ -25,10 +28,10 @@ typedef vector<pii> vpii;
 
 int store[2000000];
 
-int getN(ll n) {
-  //printf("%d\n",n);
+int getN(int n) {
   int ans;
-  if( n < 2000000 && n > 0)if(store[n]!=0) return store[n];
+  if( n < 2000000 && n > 0 && store[n]!=0)
+    return store[n];
   else if(n==1) return 1;
   else if(n%2==0) {
     ans = getN(n>>1)+1;
@@ -43,16 +46,18 @@ int getN(ll n) {
 }
 
 int main() {
-  //printf("num: %d\n",getN(22)); 
   int i,j;
   int xI, xJ;
   int mx;
   while(scanf("%d %d",&i,&j)!=-1) {
     mx = 0;
-    if(i>j) { xI = j; xJ=i; }
-    else {xI = i; xJ = j;};
+    if(i>j) {
+      xJ = i; xI = j;
+    }
+    else {
+      xJ = j; xI = i;
+    }
     FORD(x,xJ,xI) {
-      //printf("%d: %d\n",x,getN(x));
       mx = max(mx,getN(x));
     }
     printf("%d %d %d\n",i,j,mx);
